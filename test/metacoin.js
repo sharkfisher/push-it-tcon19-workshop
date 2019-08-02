7a3cf01fc97967da4b15d5bd5c6d4f97bf474b0d
Mike Seese <seesemichaelj@gmail.com>
2019-08-02 02:28:19 -0700

    introduce bug

--- test/metacoin.js
+++ test/metacoin.js
@@ -5,7 +5,7 @@ contract('MetaCoin', (accounts) => {
     const metaCoinInstance = await MetaCoin.deployed();
     const balance = await metaCoinInstance.getBalance.call(accounts[0]);
 
-    assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+    assert.equal(balance.valueOf(), 1000, "10000 wasn't in the first account");
   });
   it('should call a function that depends on a linked library', async () => {
     const metaCoinInstance = await MetaCoin.deployed();
